143da5236caec090384f8f5a683071316f5ae772
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2024-09-09 18:46:59 -0300

    main

---
+++ Course/Course/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Course
+{
+    class Program{
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char gender = 'F';
+            sbyte n1 = 127;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green";
+            object objt1 = "Alex Brown";
+            int teste = int.MinValue;
+
+            Console.WriteLine(teste);
+            Console.WriteLine(completo);
+            Console.WriteLine(gender);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome); // imutavel (segurança, simplicidade e thread safe)
+            Console.WriteLine(objt1);
+        }
+    }
+}
---
+++ SaidadaDeDados/SaidadaDeDados/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char gender = 'F';
+            int idade = 24;
+            double saldo = 2.34423;
+            string nome = "Kaua";
+
+            Console.WriteLine(gender);
+            Console.WriteLine(nome);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2"));
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
