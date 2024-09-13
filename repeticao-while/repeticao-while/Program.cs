28f7e0315f648e63951ea0e172a8362f2994c570
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2024-09-12 21:47:46 -0300

    laco

---
+++ repeticao-while/repeticao-while/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace repeticao_while {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite um numero:");
+            
+            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+
+            while (x >= 0.0) {
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
+                Console.Write("Digite outro numero:");
+
+                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            Console.WriteLine("Numero negativo " + x);
+        }
+    }
+}
