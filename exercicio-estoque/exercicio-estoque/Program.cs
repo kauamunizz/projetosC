c62757dcd12e3a1f11eb24d53a970a23f0a5f1f5
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2024-09-17 22:22:17 -0300

    17-09

---
+++ exercicio-estoque/exercicio-estoque/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace exercicio_estoque {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+    
+            Console.WriteLine("Entre os dados do produto");
+
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine("Dados do Produto: {0}, $ {1}, {2} unidades, Total: ", p.Nome, p.Preco, p.Quantidade);
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o numero de produtos a ser adicionado ao estoque:");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+
+            Console.Write("Digite o numero de produtos a ser removido do estoque:");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados Atualizados: " + p);
+        }
+    }
+}
