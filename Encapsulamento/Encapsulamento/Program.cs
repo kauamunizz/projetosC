7d096080c505128cc4c137220bd6a2ded5b025b6
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2024-09-23 20:08:25 -0300

    construtores

---
+++ Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,16 @@
+﻿using Course;
+
+namespace Encapsulamento {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto("TV", 500, 10);
+
+
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+            //p.Quantidade = -10;
+
+        }
+    }
+}
---
+++ Sobrecarga/Construtores/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            //Produto p = new Produto("TV", 500, 10);
+
+            //Console.WriteLine(p);
+            
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            /*sintaxe alternativa*/
+            Produto p2 = new Produto { 
+                Nome = "TV", 
+                Preco = 1000, 
+                Quantidade = 0 
+            };
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            
+        }
+    }
+}
